b5c6fe22abee347c8b17fcec6df17acd0eae234b
haydenanderl33 <hayden.anderl33@gmail.com>
2020-10-21 21:42:04 -0600

    3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends Component {
+//   constructor(){
+//     super();
+
+//     this.state = {
+//       listFiltered:'',
+//       myList : ['cat', 'dog', 'bat']
+//     }
+//   }
+
+//   render(){
+    
+    
+    
+//     return (
+//       // let {myList} = this.state.myList 
+      
+//       <ul>
+//        <input type="text" />
+//         <h2>{this.state.myList[0]}</h2>
+//         <h2>{this.state.myList[1]}</h2>
+//         <h2>{this.state.myList[2]}</h2>
+        
+//       </ul>
+//     )
+//   }
+
+
+
+
+
+
+
+// }
+
+// export default App
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: '',
+      myList: ['cat', 'dog', 'bat']
+    };
+  }
+
+  handleChange(notFilter) {
+    this.setState({ filterString: notFilter });
+  }
+
+  render() {
+    let myListdisplay = this.state.myList
+      .filter((element, index) => {
+        return element.includes(this.state.filterString);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {myListdisplay}
+      </div>
+    );
+  }
 }
 
 export default App;
