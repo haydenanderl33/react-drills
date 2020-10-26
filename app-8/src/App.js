a3efbf6b6e56a01dded56c3222b955df896bcedd
haydenanderl33 <hayden.anderl33@gmail.com>
2020-10-25 20:51:27 -0600

    8 done

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      pokemon: ''
+
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/45`)
+    .then(respond => {
+      this.setState({
+        pokemon: respond.data
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1>Name: {this.state.pokemon.name}</h1>
+        <h1>Height: {this.state.pokemon.height}</h1>
+        <h1>Order: {this.state.pokemon.order}</h1>
+        <h1>Id: {this.state.pokemon.id}</h1>
+
+        
+      
+
+        
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
